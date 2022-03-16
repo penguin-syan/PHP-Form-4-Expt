7eb2a737eb441534ca88af0110aff1746bfa57be
penguin-syan <yuto.mitsuta@penguin-syan.tokyo>
2022-03-16 17:35:27 +0900

    namespaceの適用

--- src/penguin_syan/php_form_4_expt/sample_phpDoc.php
+++ src/penguin_syan/php_form_4_expt/sample_phpDoc.php
@@ -1,10 +1,13 @@
 <?php
+namespace penguin_syan\php_form_4_expt;
+
 /**
  *  関数の概要
  *
  * @param int $id ユーザID
  * @param string $name ユーザ名
  */
-function sample_func(int $id, string $name){
+
+function samplefunc(int $id, string $name){
     return "ユーザIDが".$id."の人は".$name."さんです．";
 }
