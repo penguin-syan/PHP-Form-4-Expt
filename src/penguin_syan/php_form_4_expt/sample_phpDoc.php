693edf475c55df8545148d582c9d4e110e704891
penguin-syan <yuto.mitsuta@penguin-syan.tokyo>
2022-03-16 18:26:20 +0900

    phpdocを更新

--- src/penguin_syan/php_form_4_expt/sample_phpDoc.php
+++ src/penguin_syan/php_form_4_expt/sample_phpDoc.php
@@ -2,12 +2,18 @@
 namespace penguin_syan\php_form_4_expt;
 
 /**
- *  関数の概要
+ * サンプル関数
+ * 
+ * PHPDocの動作及びその結果を確認するために作成された関数．
+ * ライブラリとしては本関数に意味はなく，ある程度開発が進んだ段階で削除する．
+ * 当然，ライブラリとしてこの関数を使用することは厳禁である．
  *
+ * @deprecated PHPDocのサンプル用関数の為，非推奨どころか使用禁止．
+ * 
  * @param int $id ユーザID
  * @param string $name ユーザ名
+ * @return string ユーザIDとユーザ名を結合した文字列
  */
-
-function samplefunc(int $id, string $name){
-    return "ユーザIDが".$id."の人は".$name."さんです．";
+function sample_func(int $id, string $name){
+   return "ユーザIDが".$id."の人は".$name."さんです．";
 }
