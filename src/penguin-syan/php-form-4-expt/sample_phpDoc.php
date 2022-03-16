85924c038edf8ef22de476f4109effa5c186b020
penguin-syan <yuto.mitsuta@penguin-syan.tokyo>
2022-03-16 16:36:09 +0900

    phpdocのコメントを変更

--- src/penguin-syan/php-form-4-expt/sample_phpDoc.php
+++ src/penguin-syan/php-form-4-expt/sample_phpDoc.php
@@ -4,7 +4,6 @@
  *
  * @param int $id ユーザID
  * @param string $name ユーザ名
- * @return string 入力されたユーザIDとユーザ名を出力
  */
 function sample_func(int $id, string $name){
     return "ユーザIDが".$id."の人は".$name."さんです．";
