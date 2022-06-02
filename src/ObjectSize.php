df43f540f7a14297712aa07b1fc412ba9e57f96c
akihiro21 <akihiro8922@gmail.com>
2022-06-02 21:08:51 +0900

    [dev-006]動画タグ

---
+++ src/ObjectSize.php
@@ -0,0 +1,17 @@
+<?php
+namespace penguin_syan\php_form_4_expt;
+
+/**
+ * 画像を表示するための関数
+ * 画像サイズのパラメータ用クラス（ObjectSize）
+ *  
+ */
+class ObjectSize {
+    const SMALL = 0;
+    const MEDIUM = 1;
+    const LARGE = 2;
+    const FULL = 3;
+    const SMALL_FULL = 4;
+    const MEDIUM_FULL = 5;
+    const LARGE_FULL = 6;
+}
