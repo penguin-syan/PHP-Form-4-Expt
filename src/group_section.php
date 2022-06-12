0306dc9458b7a3030994668d97ab403f09aae1bd
akihiro21 <akihiro8922@gmail.com>
2022-06-12 20:56:39 +0900

    引数なしの関数の対応

--- src/group_section.php
+++ src/group_section.php
@@ -5,22 +5,24 @@ namespace penguin_syan\php_form_4_expt;
  * 複数の質問をまとめて扱うセクションを作成するための関数
  * 
  * 本関数を使用すると，複数の質問項目をまとめて扱うことができる．
+ * また、必要に応じてページ内で表示順序を入れ替えることができる．
  * 
  * 
  * 使用例
- * $group1 = ["random_section",
+ * $group1 = ["penguin_syan\php_form_4_expt\\random_section",
  *             [
- *               ['video','./movie.mp4', ObjectSize::SMALL_FULL , 3],
- *               ["likert_scale",'次のうち当てはまるものを選択してください．','迷わず，直感的に選択してください',['非常に同意する', '同意する', 'やや同意する', 'どちらともいえない', 'やや同意しない', '同意しない', '全く同意しない'],true],
- *               ["video",'./movie.mp4', ObjectSize::SMALL, 1]
+ *               ['penguin_syan\php_form_4_expt\\video','./movie.mp4', ObjectSize::SMALL_FULL , 1],
+ *               ["penguin_syan\php_form_4_expt\\video",'./movie.mp4', ObjectSize::SMALL, 2],
+ *               ["penguin_syan\php_form_4_expt\\video",'./movie.mp4', ObjectSize::SMALL, 3],
+ *               ['penguin_syan\php_form_4_expt\video','./movie.mp4', ObjectSize::SMALL_FULL , 4]
  *             ]
  *           ];
  *
- * $group2 = ["random_section",
+ * $group2 = ["penguin_syan\php_form_4_expt\\random_section",
  *             [
- *               ['video','./movie.mp4', ObjectSize::SMALL_FULL , 3],
- *               ["likert_scale",'次のうち当てはまるものを選択してください．','迷わず，直感的に選択してください',['非常に同意する', '同意する', 'やや同意する', 'どちらともいえない', 'やや同意しない', '同意しない', '全く同意しない'],true],
- *               ["video",'./movie.mp4', ObjectSize::SMALL, 1]
+ *               ["penguin_syan\php_form_4_expt\\likert_scale",'次のうち当てはまるものを選択してください．','迷わず，直感的に選択してください',['非常に同意する', '同意する', 'やや同意する', 'どちらともいえない', 'やや同意しない', '同意しない', '全く同意しない'],true],
+ *               ["penguin_syan\php_form_4_expt\\likert_scale",'次のうち当てはまるものを選択してください．','迷わず，直感的に選択してください',['非常に同意する', '同意する', 'やや同意する', 'どちらともいえない', 'やや同意しない', '同意しない', '全く同意しない'],true],
+ *               ["penguin_syan\php_form_4_expt\\likert_scale",'次のうち当てはまるものを選択してください．','迷わず，直感的に選択してください',['非常に同意する', '同意する', 'やや同意する', 'どちらともいえない', 'やや同意しない', '同意しない', '全く同意しない'],true]
  *             ]
  *           ];
  *
@@ -31,18 +33,34 @@ namespace penguin_syan\php_form_4_expt;
  */
 
  function group_section(array $lib_function){
-    foreach ($lib_function as $function) {
-        run($function[0],array_slice($function, 1));
+    if (is_array($lib_function[0])){
+        foreach ($lib_function as $function) {
+           group_section($function);
+        }
+    } else {
+        if (count($lib_function) > 1){
+            run($lib_function[0],array_slice($lib_function, 1));
+        } else {
+            run($lib_function[0],[]);
+        }
     }
  }
 
  function random_section(array $lib_function){
-    shuffle($lib_function);
-    foreach ($lib_function as $function) {
-        run($function[0],array_slice($function, 1));
+    if (is_array($lib_function[0])){
+        shuffle($lib_function);
+        foreach ($lib_function as $function) {
+           random_section($function);
+        }
+    } else {
+        if (count($lib_function) > 1){
+            run($lib_function[0],array_slice($lib_function, 1));
+        } else {
+            run($lib_function[0],[]);
+        }
     }
  }
 
- function run (callable $f, array $val){
-    call_user_func_array($f,$val);
+ function run (callable $callback, array $val){
+    call_user_func_array($callback, $val);
 }
